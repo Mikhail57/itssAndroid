dbcc56565df18f0d583a8ff477bc985dd706369d
Mikhail Mustakimov <misha3must@yandex.ru>
2015-12-17 07:32:52 +0800

    Add startandroid86 project

---
+++ StartAndroid86/app/src/main/java/me/mikhailmustakimov/startandroid86/MainActivity.java
@@ -0,0 +1,61 @@
+package me.mikhailmustakimov.startandroid86;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class MainActivity extends AppCompatActivity {
+
+    MyTask myTask;
+    TextView tvInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+        Button btn;
+        btn = (Button) findViewById(R.id.buttonStart);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myTask = new MyTask();
+                myTask.execute();
+            }
+        });
+    }
+
+
+
+
+    class MyTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            tvInfo.setText("Begin");
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) {
+            super.onPostExecute(result);
+            tvInfo.setText("End");
+        }
+    }
+}
